30f8a621645f4ef93cc320f843816771c26960a7
xwaizai <pxingw@gmail.com>
2021-09-08 19:31:45 +0800

    commit

--- app/src/main/java/com/example/cz2006/ui/busarrival/BusarrivalFragment.java
+++ app/src/main/java/com/example/cz2006/ui/busarrival/BusarrivalFragment.java
@@ -1,27 +1,41 @@
 package com.example.cz2006.ui.busarrival;
 
+import android.app.SearchManager;
+import android.content.Context;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import android.widget.SearchView;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 
 
+import com.example.cz2006.R;
 import com.example.cz2006.databinding.FragmentBusarrivalBinding;
 
 public class BusarrivalFragment extends Fragment {
 
+
+    private SearchView searchView = null;
+    private SearchView.OnQueryTextListener queryTextListener;
+
     private BusarrivalViewModel busarrivalViewModel;
     private FragmentBusarrivalBinding binding;
 
+
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
+
         busarrivalViewModel =
                 new ViewModelProvider(this).get(BusarrivalViewModel.class);
 
@@ -35,8 +49,46 @@ public class BusarrivalFragment extends Fragment {
                 textView.setText(s);
             }
         });
-        BusArrival busArrival = new BusArrival();
+
         return root;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setHasOptionsMenu(true);
+    }
+
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        inflater.inflate(R.menu.search, menu);
+        MenuItem searchItem = menu.findItem(R.id.search);
+        SearchManager searchManager = (SearchManager) getActivity().getSystemService(Context.SEARCH_SERVICE);
+
+        if (searchItem != null) {
+            searchView = (SearchView) searchItem.getActionView();
+        }
+        if (searchView != null) {
+            searchView.setSearchableInfo(searchManager.getSearchableInfo(getActivity().getComponentName()));
+
+            queryTextListener = new SearchView.OnQueryTextListener() {
+                @Override
+                public boolean onQueryTextChange(String newText) {
+                    Log.d("onQueryTextChange", newText);
+
+                    return true;
+                }
+                @Override
+                public boolean onQueryTextSubmit(String query) {
+                    Log.d("onQueryTextSubmit", query);
+                    BusArrival busArrival = new BusArrival();
+
+                    return true;
+                }
+            };
+            searchView.setOnQueryTextListener(queryTextListener);
+        }
+        super.onCreateOptionsMenu(menu, inflater);
     }
 
     @Override
