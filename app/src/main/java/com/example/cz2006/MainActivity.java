24fe26b53fc6a4865695beb525a374a325e4d6ea
xwaizai <pxingw@gmail.com>
2021-10-25 15:07:00 +0800

    change to covid sit rep

--- app/src/main/java/com/example/cz2006/MainActivity.java
+++ app/src/main/java/com/example/cz2006/MainActivity.java
@@ -89,8 +89,8 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
                 break;
             case R.id.nav_spaceout:
                 fragment = new CovidSitRepFragment();
-                title = getString(R.string.menu_spaceout);
-                tagName = "SpaceOut_Tag";
+                title = getString(R.string.menu_covidsitrep);
+                tagName = "CovidSitRep_Tag";
                 viewIsAtHome = false;
                 break;
             case R.id.nav_busarrival:
